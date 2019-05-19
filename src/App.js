c224106cf566329afed22b9a76f6f0ffde6c2a9c
Yassine Meftah <meftah.champart@gmail.com>
2019-05-19 23:16:32 +0000

    fix some issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
+import { Route } from 'react-router-dom';
 import SearchPage from './view/SearchPage';
 import { HomePage } from './view/HomePage';
-import { Route } from 'react-router-dom';
 
 class BooksApp extends React.Component {
 	render() {
